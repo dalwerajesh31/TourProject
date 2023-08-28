8271dccd729d9738d4f092a36e328d7949feb012
dalwerajesh31 <dalwerajesh31@gmail.com>
2023-08-28 12:09:13 +0530

    First Push of DotNet WebAPI project

---
+++ ETourProject1/ETourProject1/Repository/Appdbcontext.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ETourProject1.Repository
+
+{
+    public class Appdbcontext : DbContext
+    {
+        public Appdbcontext() { }
+
+        public Appdbcontext(DbContextOptions<Appdbcontext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(@"Data Source=(localdb)\\ProjectModels;Initial Catalog=ETourDB;Integrated Security=True;");
+        }
+
+        /* 
+       *     ____!!!! Data set of Poco Classes !!!_______
+       *     
+       * public DbSet<Customers> Customers { get; set; }
+
+        //public DbSet<Employee> Employees { get; set; }*/
+    }
+}
